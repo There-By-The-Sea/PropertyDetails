7a7f86ad49ff79fbf7bd106aee59902b0526a4a1
Phillip Korolog <phillipkorolog@Phillips-MacBook-Pro.local>
2020-03-11 11:55:38 -0600

    added file structure and a dummy mySQL database with data

---
+++ client/src/components/App.jsx
@@ -0,0 +1,36 @@
+import React from 'React';
+
+
+
+
+
+
+
+class App extends React.Component {
+    constructor(){
+        super(props);
+        this.state = {
+
+        }
+
+
+    }
+
+
+
+
+
+
+    render(){
+
+    }
+
+}
+
+
+
+
+
+
+
+export default App;
